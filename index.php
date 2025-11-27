0ddf97f8987c9d14caf7a2e19568e1d8a4d33ee0
Anderson <frrbarcelos@gmail.com>
2025-11-27 16:08:59 -0300

    index

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
 <?php header("Location: views/login.php"); ?>
-case "viagens":
-    require "./controllers/viagens.php";
-    $c = new ViagensController();
-
-    if ($action == "novo") $c->novo();
-    elseif ($action == "salvar") $c->salvar();
-    elseif ($action == "editar") $c->editar($_GET["id"]);
-    elseif ($action == "atualizar") $c->atualizar($_GET["id"]);
-    elseif ($action == "excluir") $c->excluir($_GET["id"]);
-    else $c->index();
-break;
